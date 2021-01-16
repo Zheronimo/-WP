ad0141bff0a3ce0a5c2592932ea297dcd41f792a
Zheronimo <kirichenko.e.o@gmail.com>
2021-01-16 19:58:57 +0200

    Сверстаны страницы поиска и страница 404

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,6 +6,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 	svg4everybody({});
 
+	const searchBtn = document.querySelector('.search-link');
+	const searchForm = document.querySelector('.header-search');
+	const headerNav = document.querySelector('.header__nav');
+	const headerPhone = document.querySelector('.header__phone');
+	const headerCloseBtn = document.querySelector('.header__close');
+
+	searchBtn.addEventListener('click', () => {
+		searchForm.style.display = 'block';
+		headerNav.style.display = 'none';
+		headerPhone.style.display = 'none';
+		headerCloseBtn.style.display = 'block';
+	});
+	headerCloseBtn.addEventListener('click', () => {
+		searchForm.style.display = 'none';
+		headerNav.style.display = 'block';
+		headerPhone.style.display = 'block';
+		headerCloseBtn.style.display = 'none';
+	});
+
 });
 
 //Полифил для IE11
