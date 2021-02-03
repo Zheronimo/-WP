7f9b089e18b9293dc55a970de1485521457b1e12
Zheronimo <kirichenko.e.o@gmail.com>
2021-02-03 13:36:31 +0200

    Добавлен burger и адаптивное меню

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -11,6 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	const headerNav = document.querySelector('.header__nav');
 	const headerPhone = document.querySelector('.header__phone');
 	const headerCloseBtn = document.querySelector('.header__close');
+	const burger = document.querySelector('.burger');
+	let scrollWidth = window.innerWidth - document.body.offsetWidth +'px';
+
+	burger.addEventListener('click', ()=>{
+		burger.classList.toggle('active');
+		headerNav.classList.toggle('active');
+		document.body.classList.toggle('lock');
+
+		if(headerNav.classList.contains('active')) {
+			document.body.style.paddingRight = scrollWidth;
+		} else {
+			document.body.style.paddingRight = 0;
+		}
+	});
 
 	searchBtn.addEventListener('click', () => {
 		searchForm.style.display = 'block';
